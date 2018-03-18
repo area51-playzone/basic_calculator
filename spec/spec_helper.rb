1bb4a0c8c8185ba40fcd337adde901f90bad4dd0
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2018-03-18 22:44:31 +0600

    added codecov formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 require "bundler/setup"
 require "basic_cal"
 require 'simplecov'
+require 'codecov'
 
 SimpleCov.start
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
